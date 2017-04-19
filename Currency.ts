a0d0c191da2f23ffb867365d366a62c139f69074
konaon <konaon@mail.ru>
2017-04-19 17:06:28 +0700

    Classes

--- Currency.ts
+++ Currency.ts
@@ -1,3 +1,14 @@
 /**
  * Created by konaon on 18.04.2017.
  */
+export class Currency{
+    public baseRatio: number;
+    public name: string;
+    public symbol: string;
+
+    constructor(baseRatio: number, name: string, symbol: string){
+        this.baseRatio = baseRatio;
+        this.name = name;
+        this.symbol = symbol;
+    }
+}
--- Money.ts
+++ Money.ts
@@ -1,3 +1,76 @@
 /**
  * Created by konaon on 19.04.2017.
  */
+import {Currency} from "./Currency";
+
+export class Money {
+    public amount: number;
+    public currency: Currency;
+
+    constructor(amount: number, currency: Currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public getConverted(to: Currency) : Money{
+        return new Money(
+            (this.getBaseAmount()) / to.baseRatio,
+            to
+        );
+    }
+
+    public convert(to: Currency) {
+        this.amount = (this.amount * this.currency.baseRatio) / to.baseRatio;
+        this.currency = to;
+
+        return this;
+    }
+
+    public add(money: Money)  {
+        let conMon = money.getConverted(this.currency);
+        this.amount += conMon.amount;
+
+        return this;
+    }
+
+    public subtract(money: Money) {
+        let conMon = money.getConverted(this.currency);
+        this.amount -= conMon.amount;
+
+        return this;
+    }
+
+    public multiply(multiplier: number) {
+        this.amount = this.amount * multiplier;
+
+        return this;
+    }
+
+    public isMore(money: Money) : boolean{
+        return this.getBaseAmount() > money.getBaseAmount();
+    }
+
+    public isLess(money: Money) : boolean{
+        return this.getBaseAmount() < money.getBaseAmount();
+    }
+
+    public isEqually(money: Money, accurate: number = 4) : boolean{
+        return this.getBaseAmount().toFixed(accurate) === money.getBaseAmount().toFixed(accurate);
+    }
+
+    public isMoreOrEqual(money: Money, accurate: number = 4){
+        return this.isMore(money) || this.isEqually(money, accurate);
+    }
+
+    public isLessOrEqual(money: Money, accurate: number = 4){
+        return this.isLess(money) || this.isEqually(money, accurate);
+    }
+
+    public getBaseAmount(){
+        return this.amount * this.currency.baseRatio;
+    }
+
+    public toString(accurate: number = 2){
+        return this.amount.toFixed(accurate) + " " + this.currency.symbol;
+    }
+}
